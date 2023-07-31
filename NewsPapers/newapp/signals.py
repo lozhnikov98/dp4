9af39e383ff0ee52332c37beb0d9419677f7e823
sergey lozhnikov <lozhnikovser@yandex.ru>
2023-07-31 22:39:20 +0700

    Итоговое задание D6

---
+++ NewsPapers/newapp/signals.py
@@ -0,0 +1,40 @@
+# from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+
+from .models import PostCategory
+
+
+def send_notifications(preview, pk, title, subscribers):
+    html_context = render_to_string(
+        'post_created_email.html',
+        {
+            'text': preview,
+            'link': f'http://127.0.0.1:8000//news/{pk}',
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=title,
+        body='',
+        from_email=None,
+        to=subscribers,
+    )
+
+    msg.attach_alternative(html_context, 'text/html')
+    msg.send()
+
+
+@receiver(m2m_changed, sender=PostCategory)
+def notify_about_new_post(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        categories = instance.postCategory.all()
+        subscribers = []
+        for category in categories:
+            subscribers += category.subscribers.all()
+
+        subscribers = [s.email for s in subscribers]
+
+        send_notifications(instance.preview(), instance.pk, instance.title, subscribers)
